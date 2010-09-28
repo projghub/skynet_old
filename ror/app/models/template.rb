71c755ec639f75d0309103d26b6d218c95291208
Craig Jackson <craig@projectedinc.com>
2010-09-28 13:16:46 -0600

    Using RAND instead of RANDOM.

--- ror/app/models/template.rb
+++ ror/app/models/template.rb
@@ -3,6 +3,6 @@ class Template < ActiveRecord::Base
 	has_many :serving_stats
 
 	def select_ads
-		Ad.order("RANDOM()").limit(self.positions).all
+		Ad.order("RAND()").limit(self.positions).all
 	end
 end
