0ff4a93d9fcaf414354824281468270dccfe3a5f
Craig Jackson <craig@projectedinc.com>
2010-11-02 16:23:28 -0600

    Fixed up reports to not include deleted_at in joins for reports. Cleaned up queries in reports as well.

--- ror/app/controllers/reports_controller.rb
+++ ror/app/controllers/reports_controller.rb
@@ -43,73 +43,93 @@ class ReportsController < AuthenticateController
 	end
 
 	def by_campaigns
-		@campaigns = Ad.find_by_sql(["select campaigns.*, sum(serving_stats.impressions) impressions, sum(serving_stats.clicks) clicks
-			from campaigns
-			left join ad_groups on campaigns.id = ad_groups.campaign_id
-			left join ads on ad_groups.id = ads.ad_group_id
-			left join serving_stats on ads.id = serving_stats.ad_id
-			where campaigns.account_id = ? and serving_stats.time_served >= ? and serving_stats.time_served <= ?
-			group by campaigns.id
-			order by ads.enabled desc, sum(serving_stats.impressions) desc", @auth_user.account_id, session[:reports_start_date], session[:reports_end_date]])
+		@campaigns = Campaign\
+			.select("campaigns.*, sum(serving_stats.impressions) impressions, sum(serving_stats.clicks) clicks")\
+			.joins(:ad_groups => {:ads => :serving_stats})\
+			.where(
+				"ads.deleted_at is null and campaigns.account_id = ? and serving_stats.time_served >= ? and serving_stats.time_served <= ?",
+				@auth_user.account_id,
+				session[:reports_start_date],
+				session[:reports_end_date]
+			)\
+			.group("campaigns.id")\
+			.order("ads.enabled desc, sum(serving_stats.impressions) desc")\
+			.all
 	end
 
 	def by_ad_groups
-		@ad_groups = Ad.find_by_sql(["select ad_groups.*, campaigns.enabled as campaign_enabled, sum(serving_stats.impressions) impressions, sum(serving_stats.clicks) clicks
-			from ad_groups
-			left join ads on ad_groups.id = ads.ad_group_id
-			left join serving_stats on ads.id = serving_stats.ad_id
-			left join campaigns on ad_groups.campaign_id = campaigns.id
-			where campaigns.account_id = ? and serving_stats.time_served >= ? and serving_stats.time_served <= ?
-			group by ad_groups.id
-			order by campaigns.enabled desc, sum(serving_stats.impressions) desc", @auth_user.account_id, session[:reports_start_date], session[:reports_end_date]])
+		@ad_groups = AdGroup\
+			.select("ad_groups.*, campaigns.enabled as campaign_enabled, sum(serving_stats.impressions) impressions, sum(serving_stats.clicks) clicks")\
+			.joins(:ads => :serving_stats, :campaign => {})\
+			.where(
+				"ads.deleted_at is null and campaigns.account_id = ? and serving_stats.time_served >= ? and serving_stats.time_served <= ?",
+				@auth_user.account_id,
+				session[:reports_start_date],
+				session[:reports_end_date]
+			)\
+			.group("ad_groups.id")\
+			.order("campaigns.enabled desc, sum(serving_stats.impressions) desc")\
+			.all
 	end
 
 	def by_ads
-		@ads = Ad.find_by_sql(["select ads.*, campaigns.enabled as campaign_enabled, sum(serving_stats.impressions) impressions, sum(serving_stats.clicks) clicks
-			from ads
-			left join serving_stats on ads.id = serving_stats.ad_id
-			left join ad_groups on ads.ad_group_id = ad_groups.id
-			left join campaigns on ad_groups.campaign_id = campaigns.id
-			where campaigns.account_id = ? and serving_stats.time_served >= ? and serving_stats.time_served <= ?
-			group by ads.id
-			order by ads.enabled + campaigns.enabled desc, sum(serving_stats.impressions) desc", @auth_user.account_id, session[:reports_start_date], session[:reports_end_date]])
+		@ads = Ad\
+			.select("ads.*, campaigns.enabled as campaign_enabled, sum(serving_stats.impressions) impressions, sum(serving_stats.clicks) clicks")\
+			.joins(:serving_stats => {}, :ad_group => :campaign)\
+			.where(
+				"campaigns.account_id = ? and serving_stats.time_served >= ? and serving_stats.time_served <= ?",
+				@auth_user.account_id,
+				session[:reports_start_date],
+				session[:reports_end_date]
+			)\
+			.group("ads.id")\
+			.order("ads.enabled + campaigns.enabled desc, sum(serving_stats.impressions) desc")\
+			.all
 	end
 
 	def by_publishers
-		@publishers = Ad.find_by_sql(["select publishers.*, sum(serving_stats.impressions) impressions, sum(serving_stats.clicks) clicks
-			from publishers
-			left join serving_stats on publishers.id = serving_stats.publisher_id
-			left join ads on serving_stats.ad_id = ads.id
-			left join ad_groups on ads.ad_group_id = ad_groups.id
-			left join campaigns on ad_groups.campaign_id = campaigns.id
-			where campaigns.account_id = ? and serving_stats.time_served >= ? and serving_stats.time_served <= ?
-			group by publishers.id
-			order by publishers.enabled desc, sum(serving_stats.impressions) desc", @auth_user.account_id, session[:reports_start_date], session[:reports_end_date]])
+		@publishers = Publisher\
+			.select("publishers.*, sum(serving_stats.impressions) impressions, sum(serving_stats.clicks) clicks")\
+			.joins(:serving_stats => {:ad => {:ad_group => :campaign}})\
+			.where(
+				"ads.deleted_at is null and campaigns.deleted_at is null and campaigns.account_id = ? and serving_stats.time_served >= ? and serving_stats.time_served <= ?",
+				@auth_user.account_id,
+				session[:reports_start_date],
+				session[:reports_end_date]
+			)\
+			.group("publishers.id")\
+			.order("publishers.enabled desc, sum(serving_stats.impressions) desc")\
+			.all
 	end
 
 	def by_templates
-		@templates = Ad.find_by_sql(["select templates.*, sum(serving_stats.impressions) impressions, sum(serving_stats.clicks) clicks
-			from templates
-			left join serving_stats on templates.id = serving_stats.template_id
-			left join ads on serving_stats.ad_id = ads.id
-			left join ad_groups on ads.ad_group_id = ad_groups.id
-			left join campaigns on ad_groups.campaign_id = campaigns.id
-			where campaigns.account_id = ? and serving_stats.time_served >= ? and serving_stats.time_served <= ?
-			group by templates.id
-			order by sum(serving_stats.impressions) desc", @auth_user.account_id, session[:reports_start_date], session[:reports_end_date]])
+		@templates = Template\
+			.select("templates.*, sum(serving_stats.impressions) impressions, sum(serving_stats.clicks) clicks")\
+			.joins(:serving_stats => {:ad => {:ad_group => :campaign}})\
+			.where(
+				"ads.deleted_at is null and campaigns.deleted_at is null and campaigns.account_id = ? and serving_stats.time_served >= ? and serving_stats.time_served <= ?",
+				@auth_user.account_id,
+				session[:reports_start_date],
+				session[:reports_end_date]
+			)\
+			.group("templates.id")\
+			.order("sum(serving_stats.impressions) desc")\
+			.all
 	end
 
 	def by_ad_types
-		@ad_types = Ad.find_by_sql(["select ad_types.*, sum(serving_stats.impressions) impressions, sum(serving_stats.clicks) clicks
-			from ad_types
-			left join templates on ad_types.id = templates.ad_type_id
-			left join serving_stats on templates.id = serving_stats.template_id
-			left join ads on serving_stats.ad_id = ads.id
-			left join ad_groups on ads.ad_group_id = ad_groups.id
-			left join campaigns on ad_groups.campaign_id = campaigns.id
-			where campaigns.account_id = ? and serving_stats.time_served >= ? and serving_stats.time_served <= ?
-			group by ad_types.id
-			order by sum(serving_stats.impressions) desc", @auth_user.account_id, session[:reports_start_date], session[:reports_end_date]])
+		@ad_types = AdType\
+			.select("ad_types.*, sum(serving_stats.impressions) impressions, sum(serving_stats.clicks) clicks")\
+			.joins(:templates => {:serving_stats => {:ad => {:ad_group => :campaign}}})\
+			.where(
+				"ads.deleted_at is null and campaigns.deleted_at is null and campaigns.account_id = ? and serving_stats.time_served >= ? and serving_stats.time_served <= ?",
+				@auth_user.account_id,
+				session[:reports_start_date],
+				session[:reports_end_date]
+			)\
+			.group("ad_types.id")\
+			.order("sum(serving_stats.impressions) desc")\
+			.all
 	end
 
 	protected
